9897e90e4420d5ce238f19ce7a0ee5e969242f80
CanicheAnsioso <layla.elizabeth95@gmail.com>
2021-07-21 20:25:35 -0300

    carrusel y otres

--- houseDetail.js
+++ houseDetail.js
@@ -1,9 +1,9 @@
 var houseImages = [
-	'http://127.0.0.1:5500/Imagenes/Casa11.jpeg',
-	'http://127.0.0.1:5500/Imagenes/Interior1.jpg',
-	'http://127.0.0.1:5500/Imagenes/Interior3.jpg',
-	'http://127.0.0.1:5500/Imagenes/Interior2.jpg',
-	'http://127.0.0.1:5500/Imagenes/Interior4.jpeg',
+	'http://192.168.1.40:5500/Imagenes/Casa11.jpeg',
+	'http://192.168.1.40:5500/Imagenes/Interior1.jpg',
+	'http://192.168.1.40:5500/Imagenes/Interior3.jpg',
+	'http://192.168.1.40:5500/Imagenes/Interior2.jpg',
+	'http://192.168.1.40:5500/Imagenes/Interior4.jpeg',
 ];
 
 var selectedHouseImageIndex = 0;
@@ -12,6 +12,7 @@ const onClickImage = image => {
 	const imageSRC = image.src;
 	let imageIndex = houseImages.findIndex(image => image === imageSRC);
 	if (imageIndex === -1) imageIndex = 0;
+	console.log(image, imageSRC, imageIndex, houseImages)
 	changeSelectedHouseImage(imageIndex);
 };
 
